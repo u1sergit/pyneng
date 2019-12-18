7b11e50784a0c3bf19ddd1791f855239c289a4e1
u1ser <fedetsov@gmail.com>
2019-12-18 11:02:53 +0200

    5

--- 05_basic_scripts/task_5_2.py
+++ 05_basic_scripts/task_5_2.py
@@ -19,3 +19,52 @@ Mask:
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+ip = input('ввод IP-сети в формате: 10.1.1.0/24\n')
+ip = ip.split('/')
+ip[0] = ip[0].split('.')
+print('{:<8}  {:<8}  {:<8}  {:<8}'.format(
+    ip[0][0], ip[0][1], ip[0][2], ip[0][3]))
+print('{:0>8b}  {:0>8b}  {:0>8b}  {:0>8b}'.format(
+    int(ip[0][0]), int(ip[0][1]), int(ip[0][2]), int(ip[0][3])))
+
+
+mask = int(ip[1])
+
+print('/' + str(mask))
+
+#print(int(mask / 8))
+#print(mask % 8)
+
+if ((mask - 8) > 0):
+    oct1 = '1'*8
+    mask = mask - 8
+    if ((mask - 8) > 0):
+        oct2 = '1'*8
+        mask = mask - 8
+        if ((mask - 8) > 0):
+            oct3 = '1'*8
+            oct4 = '1' * (mask - 8)
+            print('{:<8}  {:<8}  {:<8}  {:<8}'.format(
+                int(oct1, 2), int(oct2, 2), int(oct3, 2), int(oct4, 2)))
+            print('{:0>8b}  {:0>8b}  {:0>8b}  {:0>8b}'.format(
+                int(oct1, 2), int(oct2, 2), int(oct3, 2), int(oct4, 2)))
+
+        else:
+            oct3 = '1'*mask
+            print('{:<8}  {:<8}  {:<8}  {:<8}'.format(
+                int(oct1, 2), int(oct2, 2), int(oct3, 2), 0))
+            print('{:0>8b}  {:0>8b}  {:0>8b}  {:0>8b}'.format(
+                int(oct1, 2), int(oct2, 2), int(oct3, 2), 0))
+
+    else:
+        oct2 = '1'*mask
+        print('{:<8}  {:<8}  {:<8}  {:<8}'.format(
+            int(oct1, 2), int(oct2, 2), 0, 0))
+        print('{:0>8b}  {:0>8b}  {:0>8b}  {:0>8b}'.format(
+            int(oct1, 2), int(oct2, 2), 0, 0))
+
+
+else:
+    oct1 = '1'*mask
+    print('{:<8}  {:<8}  {:<8}  {:<8}'.format(int(oct1, 2), 0, 0, 0))
+    print('{:0>8b}  {:0>8b}  {:0>8b}  {:0>8b}'.format(int(oct1, 2), 0, 0, 0))
