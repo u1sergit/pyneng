60c3b68bd660c1aa848ebb557b93b71f288c8e62
u1ser <fedetsov@gmail.com>
2019-12-26 16:57:10 +0200

    7_2а

--- 07_files/task_7_2a.py
+++ 07_files/task_7_2a.py
@@ -12,4 +12,24 @@
 
 '''
 
-ignore = ['duplex', 'alias', 'Current configuration']
+ignore = ['duplex', 'alias', 'Current configuration', '!']
+import sys
+
+try:
+    with open(sys.argv[1]) as file:
+        for line in file:
+          is_ignored = False
+
+
+          for ignor in ignore:
+            if line.find(ignor) != -1:
+              is_ignored = True
+
+
+          if not is_ignored:
+            print(line.rstrip())
+                       
+except FileNotFoundError:
+    print('File ' + sys.argv[1] + ' does not exist')
+except IndexError:
+    print("You didn't enter file name")
